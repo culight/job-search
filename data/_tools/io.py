6ec925c8bb72ab49446c6e0d69119bf94bdd41f7
culight <dmoton3.14@gmail.com>
2023-11-21 13:11:50 -0600

    use log instead of print

--- data/_tools/io.py
+++ data/_tools/io.py
@@ -27,10 +27,10 @@ class GCSClient:
         bucket = self.get_bucket()
         blob = bucket.blob(destination_blob_name)
         blob.upload_from_filename(source_file_name)
-        print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+        LOGGER.info(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
     def download_blob(self, source_blob_name, destination_file_name):
         bucket = self.get_bucket()
         blob = bucket.blob(source_blob_name)
         blob.download_to_filename(destination_file_name)
-        print(f"Blob {source_blob_name} downloaded to {destination_file_name}.")
+        LOGGER.info(f"Blob {source_blob_name} downloaded to {destination_file_name}.")

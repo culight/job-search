bf120fc4be730cfbc481636144e83fe7d66a1320
culight <dmoton3.14@gmail.com>
2023-11-21 12:31:18 -0600

    start adding test file

---
+++ test/test_io.py
@@ -0,0 +1,12 @@
+"""
+CODE OWNERS: Demerrick Moton
+OBJECTIVE: Test Input/Output components
+DEVELOPER NOTES: 
+"""
+
+import logging
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
+
+# ==============================================================================

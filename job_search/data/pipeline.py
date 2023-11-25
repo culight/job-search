68bbd0a81cad1d734efb915761a56a7161d167e5
culight <dmoton3.14@gmail.com>
2023-11-25 12:25:08 -0600

    test

--- job_search/data/pipeline.py
+++ job_search/data/pipeline.py
@@ -11,7 +11,7 @@ from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.dataframe.convert import to_pcollection
 
-from extract import extraction_pipeline
+from .extract import extraction_pipeline
 
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 LOGGER = logging.getLogger(__name__)

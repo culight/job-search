9aae322ac48427908db9a4f7ab0b66f422bfb033
culight <dmoton3.14@gmail.com>
2023-11-28 10:42:12 -0600

    test

--- job_search/data/pipeline.py
+++ job_search/data/pipeline.py
@@ -6,7 +6,10 @@ DEVELOPER NOTES:
 
 import logging
 
-from .extract import extraction_pipeline
+# from .extract import extraction_pipeline
+import apache_beam as beam
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.options.pipeline_options import PipelineOptions
 
 logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
@@ -15,9 +18,20 @@ LOGGER = logging.getLogger(__name__)
 
 
 def build_pipeline(argv):
-    extraction_pipeline.run(argv)
+    # extraction_pipeline.run(argv)
+    with beam.Pipeline(options=PipelineOptions(argv)) as pipeline:
+        pass
+        # # Read in the data
+        # df = pipeline | "Read CSV" >> beam.io.ReadFromText("data/extract/extracted_jobs.csv")
+
+        # # Convert to a PCollection
+        # pcoll = to_pcollection(df)
+
+        # # Write the PCollection to a file
+        # pcoll | "Write CSV" >> beam.io.WriteToText("data/transform/transformed_jobs.csv")
 
 
 if __name__ == "__main__":
     import sys
+
     build_pipeline(sys.argv[1:])

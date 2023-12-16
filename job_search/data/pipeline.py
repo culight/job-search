a968d5f7614f99e9c04efc0c2ed05ace86399e8d
culight <dmoton3.14@gmail.com>
2023-12-16 11:26:23 -0600

    test

--- job_search/data/pipeline.py
+++ job_search/data/pipeline.py
@@ -14,6 +14,28 @@ LOGGER = logging.getLogger(__name__)
 
 # ==============================================================================
 
+jobs_schema = {
+    "fields": [
+        {"name": "job_url", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "site", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "title", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "company", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "company_url", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "location", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "job_type", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "date_posted", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "interval", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "min_amount", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "max_amount", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "currency", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "is_remote", "type": "BOOL", "mode": "NULLABLE"},
+        {"name": "num_urgent_words", "type": "INTEGER", "mode": "NULLABLE"},
+        {"name": "benefits", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "emails", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "description", "type": "STRING", "mode": "NULLABLE"},
+    ]
+}
+
 
 def build_pipeline(argv):
     with beam.Pipeline(options=PipelineOptions(argv)) as pipeline:
@@ -25,7 +47,10 @@ def build_pipeline(argv):
         LOGGER.debug(f"jobs_raw: {jobs_raw}")
 
         jobs_raw | "Write to BQ" >> beam.io.WriteToBigQuery(
-            dataset="job_search", table="raw_jobs", project="gcp-practice-325719"
+            dataset="job_search",
+            table="raw_jobs",
+            project="gcp-practice-325719",
+            schema=jobs_schema,
         )
 
 
